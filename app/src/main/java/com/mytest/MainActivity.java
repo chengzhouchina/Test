61b0381da015e09cbdcfcc7eaa37b3b58eb7ba65
chengzhou <1014003679@qq.com>
2018-12-05 17:17:54 +0800

    2018.12.5

--- app/src/main/java/com/mytest/MainActivity.java
+++ app/src/main/java/com/mytest/MainActivity.java
@@ -2,12 +2,14 @@ package com.mytest;
 
 import android.content.Intent;
 import android.database.DataSetObserver;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.ExpandableListAdapter;
@@ -16,11 +18,21 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.callback.AbsCallback;
+import com.lzy.okgo.callback.Callback;
+import com.lzy.okgo.model.HttpHeaders;
+import com.lzy.okgo.model.Progress;
+import com.lzy.okgo.model.Response;
+import com.lzy.okgo.request.base.Request;
 import com.mytest.adapterview.ViewActivity;
 import com.mytest.animator.AnimatorActivity;
 import com.mytest.customview.CustomActivity;
 import com.mytest.gridlayout.ShowBigPicActivity;
 import com.mytest.smartrefresh.SmartActivity;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -39,6 +51,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btn_adapterview;
     @BindView(R.id.btn_animator)
     Button btn_animator;
+    @BindView(R.id.btn_mmp)
+    Button btn_mmp;
+
 
 
     @BindView(R.id.tv)
@@ -50,6 +65,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        steepStatusBar();
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         initListener();
@@ -65,6 +81,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_gridlayout.setOnClickListener(this);
         btn_adapterview.setOnClickListener(this);
         btn_animator.setOnClickListener(this);
+        btn_mmp.setOnClickListener(this);
     }
 
     @Override
@@ -88,6 +105,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_animator:
                 startActivity(new Intent(this, AnimatorActivity.class));
                 break;
+            case R.id.btn_mmp:
+//                    .headers("AppId","5c061ccd77b09b472e7a528b")
+//                Map<String,Object>  map = new HashMap<>();
+//                map.put("timestamp",System.currentTimeMillis());
+//                map.put("user_id","1");
+//                map.put("merchant_transaction_id","2");
+//                map.put("payment_channel","airtime_testing");
+//                map.put("currency","IDR");
+//                map.put("amount",100);
+//                map.put("item_name","3");
+                HttpHeaders httpHeaders = new HttpHeaders();
+                httpHeaders.put("AppId","5c061ccd77b09b472e7a528b");
+                String sandbox = "https://staging.simplepayment.solutions/";
+                String production  = "https://api.simplepayment.solutions/";
+                OkGo.<String>post(sandbox+ "api/v1/create")
+                        .headers(httpHeaders)
+                        .params("timestamp",System.currentTimeMillis())
+                        .params("user_id","1")
+                        .params("merchant_transaction_id","2")
+                        .params("payment_channel","airtime_testing")
+                        .params("currency","IDR")
+                        .params("amount",100)
+                        .params("item_name","3")
+                        .execute(new Callback<String>() {
+                            @Override
+                            public void onStart(Request<String, ? extends Request> request) {
+
+                            }
+
+                            @Override
+                            public void onSuccess(Response<String> response) {
+                                Log.e("mytest", "onSuccess: " + response );
+                            }
+
+                            @Override
+                            public void onCacheSuccess(Response<String> response) {
+
+                            }
+
+                            @Override
+                            public void onError(Response<String> response) {
+
+                            }
+
+                            @Override
+                            public void onFinish() {
+
+                            }
+
+                            @Override
+                            public void uploadProgress(Progress progress) {
+
+                            }
+
+                            @Override
+                            public void downloadProgress(Progress progress) {
+
+                            }
+
+                            @Override
+                            public String convertResponse(okhttp3.Response response) throws Throwable {
+                                return null;
+                            }
+                        });
+                break;
         }
     }
 
@@ -100,6 +182,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (resultCode == RESULT_OK) {
                 tv.setText("我不好");
             }
+        }
+    }
+
+    /*
+    判断版本号，版本号小于19不好使
+  */
+    private void steepStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 透明状态栏
+            getWindow().addFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // 透明导航栏
+            getWindow().addFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         }
     }
 }
--- app/src/main/java/com/mytest/TestActivity.java
+++ app/src/main/java/com/mytest/TestActivity.java
@@ -1,49 +1,46 @@
 package com.mytest;
 
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import com.mytest.base.BaseActivity;
 
-public class TestActivity extends AppCompatActivity implements View.OnClickListener{
+import butterknife.BindView;
+
+public class TestActivity extends BaseActivity {
 
     @BindView(R.id.btn_back)
     Button btn_back;
 
-    private Handler handler  = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-
-        }
-    };
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
-        ButterKnife.bind(this);
-        initListener();
     }
 
-    private void initListener() {
+    @Override
+    public int initLayout() {
+        return R.layout.activity_test;
+    }
+
+    @Override
+    public void addListener() {
         btn_back.setOnClickListener(this);
     }
 
+    @Override
+    public void initIntent() {
+
+    }
+
+
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()){
+    public void onClick(View view) {
+        switch (view.getId()) {
             case R.id.btn_back:
                 setResult(RESULT_OK);
-                finish();
+                back();
                 break;
         }
     }
